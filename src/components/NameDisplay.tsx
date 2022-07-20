11e67edf0784d210eaa2db4242d2a0dc90723390
Wilrosmi <will.r.smith31@gmail.com>
2022-07-20 16:42:44 +0100

    Add colors to names based on sex

--- src/components/NameDisplay.tsx
+++ src/components/NameDisplay.tsx
@@ -1,9 +1,10 @@
 import NameProp from "../utils/namePropInt";
+import genderReturn from "../utils/objectGenderReturn";
 
 function NameDisplay(props: NameProp): JSX.Element {
     return (
         <>
-            {props.names.map((nameObj, index) => <p key={index}>{nameObj.name}</p>)}
+            {props.names.map((nameObj, index) => <p key={index} className={genderReturn(nameObj)}>{nameObj.name}</p>)}
         </>
     )
 }
