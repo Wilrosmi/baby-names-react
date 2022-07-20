983a21d2bb8e44f745853da5eecd8bfa5d92489d
Wilrosmi <will.r.smith31@gmail.com>
2022-07-20 17:02:32 +0100

    Prettier Formatting

--- src/utils/objectGenderReturn.test.ts
+++ src/utils/objectGenderReturn.test.ts
@@ -1,15 +1,19 @@
 import genderReturn from "./objectGenderReturn";
-import Gender from "./genderEnum"
+import Gender from "./genderEnum";
 
 test("Returns the correct string value", () => {
-    expect(genderReturn({
-        name: "Will",
-        id: 3,
-        sex: Gender.f
-    })).toBe("f");
-    expect(genderReturn({
-        name: "Smith",
-        id: 74,
-        sex: Gender.m
-    })).toBe("m");
-})
+  expect(
+    genderReturn({
+      name: "Will",
+      id: 3,
+      sex: Gender.f,
+    })
+  ).toBe("f");
+  expect(
+    genderReturn({
+      name: "Smith",
+      id: 74,
+      sex: Gender.m,
+    })
+  ).toBe("m");
+});
