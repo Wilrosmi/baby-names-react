bae4270fb61ee8a483d25f3b91a20725efefc0ae
Wilrosmi <will.r.smith31@gmail.com>
2022-07-21 10:52:40 +0100

    Refactor nameSortAlpha

--- src/utils/nameSortAlpha.ts
+++ src/utils/nameSortAlpha.ts
@@ -1,20 +1,14 @@
 import Name from "./nameInterface";
 
-const nameSortAlpha = (a: Name, b: Name) => {
+const nameSortAlpha = (a: Name, b: Name): number => {
     let i = 0;
-    while (a.name.charAt(i - 1) !== undefined && b.name.charAt(i - 1) !== undefined) {
-      if (a.name.length === i) {
-        return -1
-      } else if (b.name.length === i ) {
-        return +1
-      } else {
-        if (a.name.charCodeAt(i) === b.name.charCodeAt(i)) {
-          i += 1;
-          continue;
-        } else {
-          return a.name.charCodeAt(i) - b.name.charCodeAt(i);
-        }
+    while (a.name.length > i && b.name.length > i) {
+      if (a.name.charCodeAt(i) !== b.name.charCodeAt(i)) {
+        return a.name.charCodeAt(i) - b.name.charCodeAt(i);
+      } else if (a.name.length === i + 1 || b.name.length === i + 1) {
+        return a.name.length - b.name.length;
       }
+      i++
     }
     return NaN;
   };
