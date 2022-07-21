c61a39c6ef1c428c69d269128a41e2d807d6a823
Wilrosmi <will.r.smith31@gmail.com>
2022-07-21 11:38:37 +0100

    Add css styling to nameList

--- src/components/NameDisplay.tsx
+++ src/components/NameDisplay.tsx
@@ -2,13 +2,13 @@ import NameProp from "../utils/namePropInt";
 
 function NameDisplay(props: NameProp): JSX.Element {
   return (
-    <>
+    <div id="nameList">
       {props.names.map((nameObj) => (
         <p key={nameObj.id} className={nameObj.sex}>
           {nameObj.name}
         </p>
       ))}
-    </>
+    </div>
   );
 }
 
